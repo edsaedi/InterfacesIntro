42dd91658969c4f2ddcf46376259f243bdb8596d
Edan Saedi <edsaedi@gmail.com>
2019-02-27 16:23:51 -0800

    I learned interfaces.

---
+++ InterfacesIntro/InterfacesIntro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace InterfacesIntro
+{
+    internal interface IJumpable
+    {
+        void Jump(int height);
+    }
+
+    internal class A : IJumpable
+    {
+        public int Value;
+
+        public void Jump(int height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class B<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(){ 1, 2, 3 };
+            B<int> other = new B<int>();
+            
+
+            if(numbers.SequenceEqual(other))
+            {
+                ;
+            }
+            else
+            {
+                ;
+            }
+
+
+
+        }
+    }
+}
